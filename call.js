30ec7c01a3a840130939880119185ad4fabccf36
zhengjunxiang <18810076541@163.com>
2019-09-29 18:00:59 +0800

    update 09/29

--- call.js
+++ call.js
@@ -13,7 +13,7 @@ Function.prototype.myCall = function (context) {
   if (typeof this !== 'function') throw new TypeError('Not Function');
   context = context || window;
   context.fn = this;
-  var args = arguments.slice(1);
+  var args = [...arguments].slice(1);
   var result = context.fn(...args);
   delete context[fn];
   return result;
